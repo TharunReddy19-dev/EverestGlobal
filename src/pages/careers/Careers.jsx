d1dc0590ffba7a9bae2dadd1bf4eab8dda8f23dc
reddytharun <reddytharun367@gmail.com>
2025-08-20 19:13:03 +0530

    latest chansges

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import MainNavbar from '../../components/MainNavbar';
-import careers from '../../assets/images/careers.jpg';
-import careersImages from '../../assets/images/careerslatest.jpg';
-import SoftwareDeveloperNxt from '../careers/SoftwareDeveloperNxt';
-import careers3 from '../../assets/images/Architects.jpg';
-import './Careers.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import MainNavbar from "../../components/MainNavbar";
+import careers from "../../assets/images/careers.jpg";
+import careersImages from "../../assets/images/careerslatest.jpg";
+import SoftwareDeveloperNxt from "../careers/SoftwareDeveloperNxt";
+import careers3 from "../../assets/images/Architects.jpg";
+import "./Careers.css";
 
 const Careers = () => {
   const navigate = useNavigate();
@@ -15,33 +15,42 @@ const Careers = () => {
     {
       id: 1,
       image: careers,
-      category: "Software Developer",
-     
-      description: "Lead team of Oracle Fusion Cloud HCM developers. Design & architect Cloud HCM solutions.",
-      location: "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as required)",
+      category: "Software Developers",
+
+      description:
+        "Lead team of Oracle Fusion Cloud HCM developers. Design & architect Cloud HCM solutions.",
+      location:
+        "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as telecommuting required)",
       salary: "$149,781 per year",
-      contact: "Send resume to: gupta@everestglobalsolutionsinc.com",
-      path: "/careers/software-developer"
+      contact:
+        "Send resume to Everest Global Solutions Inc at: gupta@everestglobalsolutionsinc.com",
+      path: "/careers/software-developer",
     },
     {
       id: 2,
       image: careersImages,
-      category: "INFORMATION SYSTEM ANALYST",
-      description: "Leverage LeanIX to analyze, consolidate & validate app landscape. Some job duties can be performed from home.",
-      location: "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as required)",
+      category: "INFORMATION SYSTEM ANALYSTS",
+      description:
+        "Leverage LeanIX to analyze, consolidate & validate app landscape. Some job duties can be performed from home.",
+      location:
+        "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as required)",
       salary: "$104,894 per year",
-      contact: "Send resume to: gupta@everestglobalsolutionsinc.com",
-      path: "/careers/comp&Info-System-Mang"
+      contact:
+        "Send resume to Everest Global Solutions Inc at: gupta@everestglobalsolutionsinc.com",
+      path: "/careers/comp&Info-System-Mang",
     },
     {
       id: 3,
       image: careers3,
       category: "COMPUTER SYSTEMS ENGINEER/ARCHITECT",
-      description: "Lead team of Salesforce developers providing technical guidance, mentoring & code reviews. Design and architect Salesforce solutions.",
-      location: "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as required)",
+      description:
+        "Lead team of Salesforce developers providing technical guidance, mentoring & code reviews. Design and architect Salesforce solutions.",
+      location:
+        "Miami, FL area (Travel/relocate to various unanticipated U.S. locations as required)",
       salary: "$138,486 per year",
-      contact: "Send resume to: gupta@everestglobalsolutionsinc.com",
-      path: "/careers/computerSys-eng-aecti"
+      contact:
+        "Send resume to Everest Global Solutions Inc at : gupta@everestglobalsolutionsinc.com",
+      path: "/careers/computerSys-eng-aecti",
     },
   ];
 
@@ -51,7 +60,10 @@ const Careers = () => {
       <section className="careers-hero">
         <div className="careers-content">
           <h1 className="careers-heading">Join Our Team</h1>
-          <p className="careers-subheading">Explore exciting career opportunities in I.T. and Systems Engineering</p>
+          <p className="careers-subheading">
+            Explore exciting career opportunities in I.T. and Systems
+            Engineering
+          </p>
         </div>
       </section>
       <section className="careers-wrapper">
@@ -63,21 +75,36 @@ const Careers = () => {
                 <span className="job-category">{job.category}</span>
                 <h3 className="job-title">{job.title}</h3>
                 <div className="job-details">
-                  <p><strong>Job Description:</strong> {job.description}</p>
-                  <p><strong>Location:</strong> {job.location}</p>
-                  <p><strong>Salary:</strong> {job.salary}</p>
-                  <p><strong>Contact:</strong> {job.contact}</p>
+                  <p>
+                    <strong>Job Description:</strong> {job.description}
+                  </p>
+                  <p>
+                    <strong>Location:</strong> {job.location}
+                  </p>
+                  <p>
+                    <strong>Salary:</strong> {job.salary}
+                  </p>
+                  <p>
+                    <strong>Contact:</strong> {job.contact}
+                  </p>
                 </div>
-               {/*  <button className="apply-button" onClick={() => navigate(job.path)}>Apply Now</button> */}
-               <button className="apply-button" onClick={() => setSelectedJob(job)}>Apply Now</button>
-
+                {/*  <button className="apply-button" onClick={() => navigate(job.path)}>Apply Now</button> */}
+                <button
+                  className="apply-button"
+                  onClick={() => setSelectedJob(job)}
+                >
+                  Apply Now
+                </button>
               </div>
             </div>
           ))}
         </div>
       </section>
       {selectedJob && (
-        <SoftwareDeveloperNxt job={selectedJob} onFinish={() => setSelectedJob(null)} />
+        <SoftwareDeveloperNxt
+          job={selectedJob}
+          onFinish={() => setSelectedJob(null)}
+        />
       )}
     </>
   );
